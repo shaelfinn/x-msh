dbab7db57bdb6fcd7e0c56f04349d296dd41b083
Shael Wambui <shaelsx@gmail.com>
2026-02-20 20:07:27 +0300

    basic db setup and auth

---
+++ db/relations/auth.ts
@@ -0,0 +1,23 @@
+import { relations } from "drizzle-orm";
+import { user } from "../auth/user";
+import { session } from "../auth/session";
+import { account } from "../auth/account";
+
+export const userRelations = relations(user, ({ many }) => ({
+  sessions: many(session),
+  accounts: many(account),
+}));
+
+export const sessionRelations = relations(session, ({ one }) => ({
+  user: one(user, {
+    fields: [session.userId],
+    references: [user.id],
+  }),
+}));
+
+export const accountRelations = relations(account, ({ one }) => ({
+  user: one(user, {
+    fields: [account.userId],
+    references: [user.id],
+  }),
+}));
---
+++ db/schema.ts
@@ -0,0 +1,6 @@
+// when we have enums we'll export first and relations last
+export * from "./auth/user";
+export * from "./auth/session";
+export * from "./auth/account";
+export * from "./auth/verification";
+export * from "./relations/auth";
